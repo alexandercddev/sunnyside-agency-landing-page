c463cf69c9cf735cd1d57da35c66520e0d754a2a
Alexander Chi <arturochi2@hotmail.com>
2021-07-14 00:00:26 -0500

    Se agregó imagenes del proyecto

--- src/views/index.js
+++ src/views/index.js
@@ -5,18 +5,57 @@
 **/
 import React, { useState } from 'react';  
 
+const options = [
+    {
+        key: "about",
+        name: "About",
+    },
+    {
+        key: "services",
+        name: "Services",
+    },
+    {
+        key: "projects",
+        name: "Projects",
+    },
+    {
+        key: "contact",
+        name: "Contact",
+    }, 
+];
 
 export function SunnySide (props) { 
-    const pathName = window.location.pathname;  
+    const pathName = window.location.pathname; 
     const handleWindows = (url, index) => {
         window.open(url);
     }  
 
-    return (
-        <>
-             Sunnyside agency landing page
-        </>
-    );
+    return (<>
+        <section>
+            <header 
+                className="header"
+                style={{backgroundImage: `url("./img/desktop/image-header.jpg")`}}> 
+                <span className="logo">
+                    Sunnyside
+                </span>
+                <div id="actions">
+                    <ul className="options__list">
+                        {options.map((item, index) => {
+                            return (<li key={item.key}>
+                                <a onClick={() => { }}>
+                                    {item.name}
+                                </a>
+                            </li>)
+                        })}
+                    </ul> 
+                    <div id="toggle">
+                        <div className="toggle__md"></div>
+                    </div>
+                </div>
+            </header> 
+        </section>
+            
+    </>);
 }
 
 export default SunnySide;
