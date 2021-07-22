4823727ac9c76fe54cbd07c3b830c0ac8038e48e
Alexander Chi <arturochi2@hotmail.com>
2021-07-22 00:19:43 -0500

    Se agregó css para implementar Flexbox para la alineación de los divs

--- src/views/index.js
+++ src/views/index.js
@@ -10,23 +10,34 @@ export function SunnySide (props) {
     const pathName = window.location.pathname;
     return (<div className="main__div">    
         <header className="header" > 
-            <span className="header__logo">
-                Sunnyside
-            </span>
-            <div className="header__options">
-                <ul className="header__options__list">
-                    {options.map((item, index) => {
-                        return (<li key={item.key}>
-                            <a href={'#' + item.key}>
-                                {item.name} 
-                            </a>
-                        </li>)
-                    })}
-                </ul>  
+            <div className="header__content">
+                <span className="header__logo">
+                    Sunnyside
+                </span>
+                <div className="header__options">
+                    <ul className="header__options__list">
+                        {options.map((item, index) => {
+                            return (<li key={item.key}>
+                                <a href={'#' + item.key}>
+                                    {item.name} 
+                                </a>
+                            </li>)
+                        })}
+                    </ul>  
+                </div>
+                <div className="header__toggle">
+                    <div id="toggle">
+                        <div className="header__toggle__md"></div>
+                    </div>
+                </div>
+            </div> 
+            <div className="header__text">
+                <h1>WE ARE CREATIVE</h1>
             </div>
-            <div id="header__toggle">
-                <div className="header__toggle__md"></div>
-            </div>
+            <div className="header__arrow">
+                <div className="header__arrow__line"></div>
+                <div className="header__arrow__triangule"></div>
+            </div>  
         </header>
         {options.map((item, index) => {
             return item.component
