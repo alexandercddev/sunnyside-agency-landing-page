258cd71460679887d6a10a0b7205fb660ea70049
Alexander Chi <arturochi2@hotmail.com>
2021-07-26 00:20:44 -0500

    Se agregó style para la seción de about Se agregó más codigo JSX para about

--- src/utils/information.js
+++ src/utils/information.js
@@ -8,24 +8,38 @@ import {Services} from '../views/components/services';
 import {Projects} from '../views/components/projects';
 import {Contact} from '../views/components/contact';
 
-export const about = [
-    {
-        className: "transform",
-        subTitle: "Transform, your brand",
-        description: "We are a full-services creative agency " +
-                    "specializing in helping brands grow fast. " +
-                    "Engage your clients through compelling visual " +
-                    "that do most of the marketing for you. "
-    },
-    {
-        className: "satnd__out",
-        subTitle: "Stand out to the righjt audience",
-        description: "Using a collaborative formula of " +
-                    "designers reesarchers, photographers, " +
-                    "videographers, and copywriters, we'll " +
-                    "build and extend your brand in digital places.", 
-    }
-];
+export const about = {
+    main: [
+        {
+            className: "transform",
+            subTitle: "Transform, your brand",
+            description: "We are a full-services creative agency " +
+                        "specializing in helping brands grow fast. " +
+                        "Engage your clients through compelling visual " +
+                        "that do most of the marketing for you. "
+        },
+        {
+            className: "satnd__out",
+            subTitle: "Stand out to the righjt audience",
+            description: "Using a collaborative formula of " +
+                        "designers reesarchers, photographers, " +
+                        "videographers, and copywriters, we'll " +
+                        "build and extend your brand in digital places.", 
+        }
+    ],
+    footer: [
+        {
+            className: "graphic",
+            subTitle: "Graphic Design",
+            description: "Great design makes you memorable. We deliver atwork that underscore your brand message and potencial client´ attention"
+        },
+        {
+            className: "photography",
+            subTitle: "Photography",
+            description: "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.", 
+        }
+    ]
+};
 
 export const options = [
     {
--- src/views/components/about.js
+++ src/views/components/about.js
@@ -16,19 +16,30 @@ export function About (props) {
         <section 
             id={props.id}   
             className="section__about" >
-                {about.map((item, index) => {
-                    return (
-                        <div className={"about__content about__" + item.className }>
-                            <div className="about__text">
-                                <h2 className="about__sub__title">{item.subTitle}</h2>
-                                <p className="about__p">{item.description}</p>
-                            </div>
-                            <div className="about__img">
-                                <img className={item.className}></img>
-                            </div>
-                        </div>
-                    )
-                })}
+            {about.main.map((item, index) => {
+                return (<div key={"about" + index} className={"about__content about__" + item.className }>
+                    <div className="about__text">
+                        <h2 className="about__sub__title">{item.subTitle}</h2>
+                        <p className="about__p">{item.description}</p>
+                        <a href="#" className="about__learn__more">Learn more</a>
+                    </div>
+                    <div className="about__img">
+                        <img className={item.className}></img>
+                    </div>
+                </div>)
+            })} 
+            <div className={"about__footer__content"}>
+            {about.footer.map((item, index) => {
+                return (<div key={"about__footer__img" + index} className={"about__footer__img about__footer__" + item.className }>
+                    <img className={item.className}></img>
+                    <div className="about__footer__img_content">
+                        <h2 className="about__footer__sub__title">{item.subTitle}</h2>
+                        <p className="about__footer__p">{item.description}</p>
+                    </div>
+                </div>)
+            })}
+            </div>
+            
         </section>
     </>);
 }
